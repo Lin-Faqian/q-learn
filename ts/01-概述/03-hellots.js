501a260246cfef0179ba89b1fe3ca53d47fc1fa6
林发谦 <1397343419@qq.com>
2022-05-14 01:54:34 +0800

    初步了解ts

---
+++ ts/01-概述/03-hellots.js
@@ -0,0 +1,6 @@
+function sayHello(key) {
+    return "hello" + key;
+}
+console.log(sayHello('TS'));
+// 使用ts编译
+// tsc hellots.ts
---
+++ ts/01-概述/03-hellots.ts
@@ -0,0 +1,11 @@
+function sayHello(key: string){
+  return "hello" + key
+}
+
+console.log(sayHello('TS'))
+
+// 1. 使用ts编译
+// tsc hellots.ts
+
+// 2. 编译完成使用node执行js文件
+// helloTS
